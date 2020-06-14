3f97e727d77fa117745dab860d09f4c633b51b53
pritiranjanswain <swainpri273@gmail.com>
2020-06-14 19:36:26 +0530

    Update SecurityConfig.java

--- src/main/java/com/javatechie/jwt/api/config/SecurityConfig.java
+++ src/main/java/com/javatechie/jwt/api/config/SecurityConfig.java
@@ -43,6 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors().disable();
         http.csrf().disable().authorizeRequests().antMatchers("/authenticate")
                 .permitAll().anyRequest().authenticated()
                 .and().exceptionHandling().and().sessionManagement()
